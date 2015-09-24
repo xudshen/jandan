b9c095438f56fdc7f5aacba42cda20441a675371
xudshen <xudshen@hotmail.com>
2015-09-24 17:20:59 +0800

    add some pre-defined file

---
+++ app/src/main/java/info/xudshen/jandan/JandanApp.java
@@ -0,0 +1,18 @@
+package info.xudshen.jandan;
+
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by xudshen on 15/9/24.
+ */
+public class JandanApp extends MultiDexApplication {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+
+}
