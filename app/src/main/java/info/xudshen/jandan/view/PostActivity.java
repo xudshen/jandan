ff64319b0f27c32e32dc50f90d2a371d22db4019
xudshen <xudshen@hotmail.com>
2015-09-25 17:21:57 +0800

    add fragment

--- app/src/main/java/info/xudshen/jandan/view/PostActivity.java
+++ app/src/main/java/info/xudshen/jandan/view/PostActivity.java
@@ -1,10 +1,13 @@
 package info.xudshen.jandan.view;
 
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -13,11 +16,13 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import info.xudshen.jandan.R;
 
-public class PostActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity implements PostsFragment.OnFragmentInteractionListener {
     @Bind(R.id.app_toolbar)
     Toolbar toolbar;
     @Bind(R.id.drawer_layout)
     DrawerLayout drawerLayout;
+    @Bind(R.id.nvView)
+    NavigationView navigationView;
     ActionBarDrawerToggle drawerToggle;
 
     @Override
@@ -30,6 +35,48 @@ public class PostActivity extends AppCompatActivity {
 
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
         drawerLayout.setDrawerListener(drawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            Fragment fragment = null;
+            Class fragmentClass = null;
+            switch (menuItem.getItemId()) {
+                case R.id.nav_posts: {
+                    fragmentClass = PostsFragment.class;
+                    break;
+                }
+                case R.id.nav_pics: {
+                    fragmentClass = PostsFragment.class;
+                    break;
+                }
+                case R.id.nav_jokes: {
+                    fragmentClass = PostsFragment.class;
+                    break;
+                }
+                case R.id.nav_movies: {
+                    fragmentClass = PostsFragment.class;
+                    break;
+                }
+                default:
+                    fragmentClass = PostsFragment.class;
+            }
+
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+            // Highlight the selected item, update the title, and close the drawer
+            menuItem.setChecked(true);
+            setTitle(menuItem.getTitle());
+            drawerLayout.closeDrawers();
+
+            return true;
+        });
     }
 
     @Override
@@ -54,4 +101,8 @@ public class PostActivity extends AppCompatActivity {
         drawerToggle.syncState();
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
---
+++ app/src/main/java/info/xudshen/jandan/view/PostsFragment.java
@@ -0,0 +1,83 @@
+package info.xudshen.jandan.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.xudshen.jandan.R;
+
+public class PostsFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    // TODO: Rename and change types and number of parameters
+    public static PostsFragment newInstance(String param1, String param2) {
+        PostsFragment fragment = new PostsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (OnFragmentInteractionListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
