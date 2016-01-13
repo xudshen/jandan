9896c2dab0e3f93e1bb3d7c986c526584c78d7ea
xudshen <xudshen@hotmail.com>
2016-01-13 21:17:32 +0800

    After set an adapter to the ViewPager

--- app/src/main/java/info/xudshen/jandan/view/fragment/PostHubFragment.java
+++ app/src/main/java/info/xudshen/jandan/view/fragment/PostHubFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.florent37.materialviewpager.MaterialViewPager;
+import com.github.florent37.materialviewpager.header.HeaderDesign;
 import com.mikepenz.materialdrawer.Drawer;
 
 import org.slf4j.Logger;
@@ -57,6 +58,28 @@ public class PostHubFragment extends BaseFragment {
         viewPager.getViewPager().setAdapter(new PostHubFragmentPagerAdapter(getFragmentManager(),
                 getActivity()));
 
+        viewPager.setMaterialViewPagerListener(page -> {
+            switch (page) {
+                case 0:
+                    return HeaderDesign.fromColorResAndUrl(
+                            R.color.green,
+                            "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg");
+                case 1:
+                    return HeaderDesign.fromColorResAndUrl(
+                            R.color.blue,
+                            "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg");
+                case 2:
+                    return HeaderDesign.fromColorResAndUrl(
+                            R.color.cyan,
+                            "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg");
+            }
+            //execute others actions if needed (ex : modify your header logo)
+            return null;
+        });
+
+        viewPager.getViewPager().setOffscreenPageLimit(viewPager.getViewPager().getAdapter().getCount());
+        //After set an adapter to the ViewPager
+        viewPager.getPagerTitleStrip().setViewPager(viewPager.getViewPager());
 //        TabLayout tabLayout = (TabLayout) getActivity().findViewById(R.id.sliding_tabs);
 //        tabLayout.removeAllTabs();
 //        tabLayout.setupWithViewPager(viewPager);
