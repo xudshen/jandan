f4c97663939b92fcce4480f80662c7dc94833c01
xudshen <xudshen@hotmail.com>
2016-02-20 22:46:16 +0800

    add User-Agent for APIAdapter

--- data/src/main/java/info/xudshen/jandan/data/api/ApiAdapter.java
+++ data/src/main/java/info/xudshen/jandan/data/api/ApiAdapter.java
@@ -1,6 +1,11 @@
 package info.xudshen.jandan.data.api;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -15,7 +20,22 @@ public class ApiAdapter {
     static <T> T createRetrofitService(final Class<T> clazz, final String baseUrl) {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Interceptor.Chain chain) throws IOException {
+                        Request original = chain.request();
+
+                        Request request = original.newBuilder()
+                                .header("User-Agent", "Jandan Android App V3.0.0.2")
+                                .method(original.method(), original.body())
+                                .build();
+
+                        return chain.proceed(request);
+                    }
+                })
+                .addInterceptor(interceptor)
+                .build();
 
         final Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
