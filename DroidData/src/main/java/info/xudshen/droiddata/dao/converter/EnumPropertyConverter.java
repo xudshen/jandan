eef9e9711bc05b745540c7130169aa570b4d49e8
xudshen <xudshen@hotmail.com>
2016-02-26 16:54:58 +0800

    fix EnumPropertyConverter bug

--- DroidData/src/main/java/info/xudshen/droiddata/dao/converter/EnumPropertyConverter.java
+++ DroidData/src/main/java/info/xudshen/droiddata/dao/converter/EnumPropertyConverter.java
@@ -21,14 +21,31 @@ public class EnumPropertyConverter<T extends Enum<T>> implements IPropertyConver
 
     @Override
     public T convertToEntityProperty(Class<T> clazz, String value) {
-//        if (BaseStatus.class.isAssignableFrom(clazz)) {
-//            return (T) BaseStatus.valueOf(value);
-//        }
-        Log.e(this.getClass().getName(), String.format("can not convert (value=%s) to (type=%s)", value, clazz.getName()));
+        //TODO: check Enums.getIfPresent(Blah.class, "A")
+        try {
+            return valueOfIgnoreCase(clazz, value);
+        } catch (Exception e) {
+            Log.e(this.getClass().getName(), String.format("can not convert (value=%s) to (type=%s)", value, clazz.getName()));
+        }
         return null;
     }
 
     public static String getName(String className) {
         return "info.xudshen.droiddata.dao.converter.EnumPropertyConverter<" + DaoUtil.getClassnameFromFullyQualified(className) + ">";
+    }
+
+    public static <T extends Enum<T>> T valueOfIgnoreCase(
+            Class<T> enumeration, String name) {
+
+        for (T enumValue : enumeration.getEnumConstants()) {
+            if (enumValue.name().equalsIgnoreCase(name)) {
+                return enumValue;
+            }
+        }
+
+        throw new IllegalArgumentException(String.format(
+                "There is no value with name '%s' in Enum %s",
+                name, enumeration.getName()
+        ));
     }
 }
