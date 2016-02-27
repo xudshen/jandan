6c1ae28b790550884b4089857e5a34d212f6e427
xudshen <xudshen@hotmail.com>
2016-02-27 14:20:13 +0800

    fix gif size bug

--- app/src/main/java/info/xudshen/jandan/view/component/ProgressImageView.java
+++ app/src/main/java/info/xudshen/jandan/view/component/ProgressImageView.java
@@ -15,6 +15,7 @@ import com.bumptech.glide.DrawableTypeRequest;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.bumptech.glide.request.target.Target;
 import com.squareup.picasso.Picasso;
@@ -86,8 +87,9 @@ public class ProgressImageView extends RelativeLayout {
             }
 
             request.diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                    .centerCrop() // needs explicit transformation, because we're using a custom target
-                    .crossFade().into(target);
+                    .fitCenter() // needs explicit transformation, because we're using a custom target
+                    .crossFade()
+                    .into(target);
         } else {
             progressTextView.setVisibility(GONE);
             progressBar.setVisibility(GONE);
